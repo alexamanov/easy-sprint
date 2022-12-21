f8c12a7b2218432f7cc38d90ebfd5339f558c3b5
Alexander Amanov <alexander.amanov@amasty.com>
2022-12-21 19:17:40 +0300

    type

--- src/Controller/Core/Crud/AbstractCrudWrapperController.php
+++ src/Controller/Core/Crud/AbstractCrudWrapperController.php
@@ -3,12 +3,15 @@
 namespace App\Controller\Core\Crud;
 
 use App\Security\StandardAuthenticator;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractCrudWrapperController extends AbstractCrudController
 {
-    public function index(AdminContext $context)
+    public function index(AdminContext $context): KeyValueStore|RedirectResponse|Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute(StandardAuthenticator::LOGIN_ROUTE);
